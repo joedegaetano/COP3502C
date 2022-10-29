a251e8985583c84c17e437ef9b9fc1f759289f24
joedegaetano <j.degaetano@ufl.edu>
2022-10-29 09:24:52 -0400

    modify program to allow user to subtract 2 numbers after adding 2 numbers

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def main():
-    a = int(input('Enter number 1: '))
-    b = int(input('Enter number 2: '))
+    a = int(input('Enter number 1 to add: '))
+    b = int(input('Enter number 2 to add: '))
 
     sum = a + b
     print(sum)
 
+    c = int(input('Enter number 1 to subtract: '))
+    d = int(input('Enter number 2 to subtract: '))
+
+    sub = a - b
+    print(sub)
 
 if __name__ == '__main__':
     main()
